9550318f093834acf6342693a8053ef4cbf1b402
Mathieu Boulianne <mathieu.boulianne@blacheyong.com>
2023-06-01 16:50:33 -0400

    Styles for .btn, .btn-secondary and disabled state

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,37 @@ module.exports = {
   content: ["./src/**/*.{js,scss}", "index.html"],
   theme: {
     extend: {
+      borderWidth: {
+        '1': '1px'
+      },
       colors: {
         'bell-primary': '#00549A',
         'blue' : {
           'dark': '#00215E',
           'medium': '#003778',
-          'light': '#C2CEDF'
+          'light': '#C2CEDF',
+          'electric': '#0075FF'
+        },
+        'disabled': {
+          'dark': '#8D8D8D',
+          'light': '#BABEC2'
         }
+      },
+      fontFamily: {
+        'base': ['Arial', 'Helvetica', 'sans-serif'],
+      },
+      fontSize: {
+        md: ['15px', '16px']
+      },
+      outlineOffset: {
+        5: '5px'
+      },
+      padding: {
+        '10px': '10px',
+        '20px': '20px'
+      },
+      ringOffsetWidth: {
+        5: '5px'
       }
     }
   },
