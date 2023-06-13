cb27582d8676c64f9ce8170c9418df474dee8be4
Mathieu Boulianne <mathieu.boulianne@blacheyong.com>
2023-06-13 16:59:08 -0400

    test default typo styles + svgToDataUri + form plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
+const svgToDataUri = require('mini-svg-data-uri')
+
 const { filterArrayTokensByType, filterTokensByType } = require("./fns");
 const tokens = require("./output/_tokens.json")
 
@@ -10,16 +12,18 @@ export const content = ["./src/**/*.{js,scss}", "index.html"]
 export const darkMode = 'class'
 export const theme = {
   extend: {
-    backgroundImage: {
-      'dropdown-arrow': 'url("./svg/icon-dropdown-arrow.svg")'
-    },
+    colors: colorTokens.colors,
+    backgroundImage: (theme) => ({
+      'dropdown-arrow': `url("${svgToDataUri(
+        `<svg fill='none' height='11' viewBox='0 0 10 11' width='10' xmlns='http://www.w3.org/2000/svg'><path clip-rule='evenodd' d='m5 8.65918 4.5-5h-9z' fill='${theme('colors.blue.medium')}' fill-rule='evenodd'/></svg>`
+      )}")`,
+    }),
     borderRadius: {
       '3px': '3px'
     },
     borderWidth: {
       '1': '1px'
     },
-    colors: colorTokens.colors,
     fontFamily: {
       'base': ['Arial', 'Helvetica', 'sans-serif'],
       'bell-slim': ['bell-slim', 'Helvetica', 'Arial', 'sans-serif']
@@ -54,6 +58,26 @@ export const theme = {
     },
     screens: {
       lg: '992px'
-    }
+    },
+    typography: (theme) => ({
+      DEFAULT: {
+        css: {
+          h1: {
+            color: theme('colors.gray.dark')
+          },
+          p: {
+            fontSize: theme('fontSize.body-default[0]'),
+            fontWeight: theme('fontSize.body-default[1].fontWeight'),
+            letterSpacing: theme('fontSize.body-default[1].letterSpacing'),
+            lineHeight: theme('fontSize.body-default[1].lineHeight'),
+          }
+        }
+      }
+    })
   }
 }
+
+export const plugins = [
+  require('@tailwindcss/typography'),
+  require('@tailwindcss/forms'),
+]
